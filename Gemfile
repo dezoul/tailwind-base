ad74fc93e2f5a8b2fc7e5a2829c4103e072646fc
dezoul <aussieme@gmail.com>
2025-09-16 13:39:00 +0800

    updated some defaults for tailwind and netlify

--- Gemfile
+++ Gemfile
@@ -3,3 +3,7 @@ source "https://rubygems.org"
 gem "jekyll", "4.3.3"
 gem "webrick"
 
+# for netlify
+gem 'logger'
+gem 'csv'
+gem 'base64'
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,23 @@ module.exports = {
     './*.{html,js,md}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: 'var(--blue)',
+        cream: 'var(--cream)',
+        yellow: 'var(--yellow)',
+        rust: 'var(--rust)',
+        
+      },
+      fontSize: {
+        tiny: '0.8rem', 
+        sm:   '1rem',   
+        md:   '2rem',  
+        lg:   '3rem',  
+        xl:   '4rem', 
+        '2xl':'5rem', 
+      },
+    },
   },
   plugins: [],
 }
